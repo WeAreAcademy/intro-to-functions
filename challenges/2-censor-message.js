ab57f29af487a6b699a7c635f7cf880225420a34
Nobeen <n.islam.1@outlook.com>
2022-02-21 17:55:51 +0000

    Yarn and eslint fully working

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -1,21 +1,21 @@
-"use strict"
+"use strict";
 
-function censorMessage(string,banList){
-    let rawWords = string.split(" ")
-    let scrubbedWords = []
+function censorMessage(string, banList) {
+  let rawWords = string.split(" ");
+  let scrubbedWords = [];
 
-    for(let word of rawWords){
-        let lowerCaseWord = word.toLowerCase()
-        if(banList.includes(lowerCaseWord) === true){
-            scrubbedWords.push("*****")
-        }else{
-            scrubbedWords.push(lowerCaseWord)
-        }
+  for (let word of rawWords) {
+    let lowerCaseWord = word.toLowerCase();
+    if (banList.includes(lowerCaseWord) === true) {
+      scrubbedWords.push("*****");
+    } else {
+      scrubbedWords.push(lowerCaseWord);
     }
-    let scrubbedMessage = scrubbedWords.join(" ")
-    return scrubbedMessage
+  }
+  let scrubbedMessage = scrubbedWords.join(" ");
+  return scrubbedMessage;
 }
 //TEST
-console.log(censorMessage("don't mess with cats",["mess"]))
-console.log(censorMessage("Are you sure that this is safe",["are","is"]))
-console.log(censorMessage("I CANNOT HELP YOU",["ARE","YOU"]))
+console.log(censorMessage("don't mess with cats", ["mess"]));
+console.log(censorMessage("Are you sure that this is safe", ["are", "is"]));
+console.log(censorMessage("I CANNOT HELP YOU", ["ARE", "YOU"]));
