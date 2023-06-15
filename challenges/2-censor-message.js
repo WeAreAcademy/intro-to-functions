677e3e0d21e0f867e46ff276c50e7c96661a6527
Beth <bethan.hp@gmail.com>
2023-06-15 13:11:48 +0100

    started 2

---
+++ challenges/2-censor-message.js
@@ -0,0 +1,29 @@
+/*
+Write a function that, given a full message and a list of words, returns a single string that represents a 'censored' version of the original message: 
+all words from the list must be scrubbed out and replaced with five asterisks (*****).
+
+The censor must scrub out matches that are case insensitive.
+
+You may assume that each string in the list is a lowercase single word.
+You may assume that there is no punctuation apart from spaces in the full message.
+
+function censorMessage
+convert string to list by space
+censoredOutput = []
+    for word in list
+        if word in censored list
+            censoredOutput.push('*' * 5)
+        censoredOutput.push(list[word])
+convert censoredOutput back to string
+return censoredOutputStr
+*/
+
+function censoredMessage(list, censorList){
+    
+}
+
+
+// test cases:
+console.log(censorMessage("don't mess with cats", ["mess"]), " should be don't ***** with cats")
+console.log(censorMessage("Are you sure that this is safe", ["are", "is"]), "should be ***** you sure that this ***** safe")
+console.log(censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"])," should be I CANNOT ***** *****")
